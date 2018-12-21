1f405c517cc5eee9f0456320f7ee1cb36df1a1e6
shujiM <shuji-munakata@gmocloud.com>
2018-12-21 14:48:30 +0900

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 //Wweb GL test
 onload = function(){
-    
-    //canvasエレメントを取得
+    // canvasエレメントを取得
     var c = document.getElementById('canvas');
-    c.width = 1000;
-    c.height = 500;
-    
-    //webglコンテキストを取得
-    var gl = c.getContext('webgl') || c.getcontext('experimental-webgl');
-    
-    //canvasを黒でクリア(初期化)する
-    gl = c.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl = c,clear(gl.COLOR_BUFFER_BIT);
+    c.width = 500;
+    c.height = 300;
+
+    // webglコンテキストを取得
+    var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+
+    // canvasを黒でクリア(初期化)する
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 };
